175f67a41f20e64bd912cdbfa2de13dd5975bafe
AlaaAref <alaa.alaa5052@gmail.com>
2025-11-07 15:41:38 +0200

    environment and angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,9 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true
+  production: true,
+  baseurl: 'http://admindoctormanagement.hgtechnologygroup.net/api',
+  baseAttatchementUrl: 'http://attachments.hgtechnologygroup.net/',
+  apiVersion: 'v1',
+  attachmentURL: 'http://attachments.hgtechnologygroup.net/api/v1/'
 };
